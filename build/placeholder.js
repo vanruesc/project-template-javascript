a6ca13d7efd2b0f6dfc6441d0ad9c7b3189a823a
Raoul v. R <vanruesc@users.noreply.github.com>
2018-12-23 11:07:05 +0100

    Fresh build.

--- build/placeholder.js
+++ build/placeholder.js
@@ -1,14 +1,13 @@
 /**
- * placeholder v0.0.0 build Mon Jul 02 2018
+ * placeholder v0.0.0 build Sun Dec 23 2018
  * https://github.com/username/placeholder
  * Copyright 2018 username, Zlib
  */
-(function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+(function (factory) {
 	typeof define === 'function' && define.amd ? define(factory) :
-	(factory());
-}(this, (function () { 'use strict';
+	factory();
+}(function () { 'use strict';
 
 
 
-})));
+}));
